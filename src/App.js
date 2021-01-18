e77f88d3466f20cb310205e80b68face505ee9c3
Juĺia Sperandio <juhsperandio@hotmail.com>
2021-01-18 11:19:50 -0300

    fix: retirado Container do footer

--- src/App.js
+++ src/App.js
@@ -1,52 +1,43 @@
-// import "./App.css";
 import styled from "styled-components";
 import Routes from "./routes";
 
 function App() {
   return (
     <>
-      <Container>
-        {/* <header></header> */}
-        <Routes></Routes>
-        <footer>
-          <a href="https://www.linkedin.com/in/adilson-dias-6a0a18170/">
-            Adilson Dias
-          </a>
+      <Routes></Routes>
+      <Footer>
+        <a href="https://www.linkedin.com/in/adilson-dias-6a0a18170/">
+          Adilson Dias
+        </a>
 
-          <a href="https://www.linkedin.com/in/amanda-gon%C3%A7alves-6a9a82a3/">
-            Amanda Gonçalves
-          </a>
+        <a href="https://www.linkedin.com/in/amanda-gon%C3%A7alves-6a9a82a3/">
+          Amanda Gonçalves
+        </a>
 
-          <a href="https://www.linkedin.com/in/j%C3%BAlia-sperandio-b31493aa/">
-            Júlia Sperandio
-          </a>
+        <a href="https://www.linkedin.com/in/j%C3%BAlia-sperandio-b31493aa/">
+          Júlia Sperandio
+        </a>
 
-          <a href="https://www.linkedin.com/in/lucasfsilva2310/">
-            Lucas Ferreira
-          </a>
+        <a href="https://www.linkedin.com/in/lucasfsilva2310/">
+          Lucas Ferreira
+        </a>
 
-          <a href="https://www.linkedin.com/in/renan-nunes-2538ab1b2/">
-            Renan Nunes
-          </a>
-        </footer>
-      </Container>
+        <a href="https://www.linkedin.com/in/renan-nunes-2538ab1b2/">
+          Renan Nunes
+        </a>
+      </Footer>
     </>
   );
 }
 
 export default App;
 
-export const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-
-  footer {
-    width: 100%;
-    height: 5%;
-    background-color: #ff9f1c;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    font-size: 0.8em;
-  }
+export const Footer = styled.footer`
+  width: 100%;
+  height: 4vh;
+  background-color: #ff9f1c;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  font-size: 0.8em;
 `;
