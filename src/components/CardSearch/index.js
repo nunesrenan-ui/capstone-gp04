c55f49ded9dfc141b6c78a834bb5cb641678103b
Adilson Dias <adilsondias@outlook.com>
2021-01-19 11:25:57 -0300

    chore: atualizaçao resposividade pagina feed

--- src/components/CardSearch/index.js
+++ src/components/CardSearch/index.js
@@ -1,49 +1,16 @@
 import CardCategory from "../../components/CardCategory";
+import { CustomSuggestions } from "../../components/SearchInput";
+import { SearchButton } from "../../components/SearchButton";
 
 import { Card, CardInput, CardTop } from "./style";
-
-import { grommet, Form, Box, Button, Grommet, TextInput, Text } from "grommet";
-// import { grommet } from "grommet/themes";
-
-const coloredButton = {
-  button: {
-    border: {
-      color: "none",
-    },
-    color: { dark: "accent-1", light: "dark-2" },
-    primary: {
-      color: "#D3F3B7",
-    },
-  },
-};
-
-const Input = {
-  button: {
-    border: {
-      color: "none",
-    },
-    color: { dark: "accent-1", light: "dark-2" },
-    primary: {
-      color: "#D3F3B7",
-    },
-  },
-};
 
 const CartSearch = () => {
   return (
     <>
       <Card>
         <CardTop>
-          <Box round>
-            <CardInput>
-              <TextInput name="name" placeholder="Buscar doações" />
-            </CardInput>
-          </Box>
-          <Grommet theme={coloredButton}>
-            <Box align="end" pad="medium">
-              <Button primary label="Search" />
-            </Box>
-          </Grommet>
+          <CustomSuggestions />
+          <SearchButton />
         </CardTop>
         <CardCategory />
       </Card>
--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -1,11 +1,13 @@
 import CardSearch from "../../components/CardSearch";
 import CardItem from "../../components/CardItem";
+
+import { BodyPage } from "./style";
 const Feed = () => {
   return (
-    <>
+    <BodyPage>
       <CardSearch />
       <CardItem />
-    </>
+    </BodyPage>
   );
 };
 
