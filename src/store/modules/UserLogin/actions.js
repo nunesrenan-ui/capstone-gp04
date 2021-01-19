4f298ed64ab6ca3a14a135a569867641f5e59d81
Lucas Ferreira <lucasfsilva2310gmail.com>
2021-01-19 17:22:29 -0300

    Requisição Login Criada.

--- src/store/modules/UserLogin/actions.js
+++ src/store/modules/UserLogin/actions.js
@@ -2,10 +2,3 @@ export const dataLogin = (data) => ({
   type: "DATA_LOGIN",
   payload: data,
 });
-
-// export const tokenLogin = (data) => ({
-//   type: "TOKEN_LOGIN",
-//   token: data,
-// });
-
-// deixar só uma action
