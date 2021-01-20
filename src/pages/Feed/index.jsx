f100d177aa7d4a02df453a09dd2b506c271ac111
Lucas Ferreira <lucasfsilva2310gmail.com>
2021-01-20 15:03:02 -0300

    Verificação de token em páginas com usuário logado realizado.

--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -1,13 +1,22 @@
 import CardSearch from "../../components/CardSearch";
 import CardItem from "../../components/CardItem";
 
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { motion } from "framer-motion";
 import Footer from "../../components/Footer";
-
+import { useEffect } from "react";
 import { Header, Container } from "./style";
-
+import { useSelector } from "react-redux";
 const Feed = () => {
+  const history = useHistory();
+  const checkToken = useSelector((state) => state.loginData.token);
+
+  useEffect(() => {
+    if (!checkToken) {
+      history.push("/");
+    }
+  }, [checkToken]);
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,12 +1,22 @@
 import ProfileSideBar from "../../components/PofileSideBar/index.js";
 import ProfileTabs from "../../components/ProfileTabs/index.js";
 import { Container } from "./style";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { motion } from "framer-motion";
 import Footer from "../../components/Footer";
 import styled from "styled-components";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const Profile = () => {
+  const history = useHistory();
+  const checkToken = useSelector((state) => state.loginData.token);
+
+  useEffect(() => {
+    if (!checkToken) {
+      history.push("/");
+    }
+  }, [checkToken]);
   return (
     <motion.div
       initial={{ opacity: 0 }}
