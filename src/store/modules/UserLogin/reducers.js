33c5297767626ef6b3cb84ea39b24b6bb0778435
@NunesRenan <renannunes2008@hotmail.com>
2021-01-20 18:28:35 -0300

    fix: conflitos no git resolvidos

--- src/store/modules/UserLogin/reducers.js
+++ src/store/modules/UserLogin/reducers.js
@@ -2,10 +2,6 @@ const loginReducer = (state = {}, action) => {
   switch (action.type) {
     case "DATA_LOGIN":
       return { ...state, ...action.data };
-<<<<<<< HEAD
-=======
-
->>>>>>> ea7a1cf2c331515205f4219222be9fa2845adaba
     default:
       return state;
   }
