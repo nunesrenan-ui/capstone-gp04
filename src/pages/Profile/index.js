0e4803037a762f2efbe791ce61cdd750700bc8d7
Juĺia Sperandio <juhsperandio@hotmail.com>
2021-01-21 13:17:42 -0300

    Profile: CSS finalizado

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,6 +1,6 @@
-import ProfileSideBar from "../../components/PofileSideBar/index.js";
+import ProfileSideBar from "../../components/ProfileSideBar/index.js";
 import ProfileTabs from "../../components/ProfileTabs/index.js";
-import { DivCentral } from "./style";
+import { DivCentral, Container } from "./style";
 import { Link, useHistory } from "react-router-dom";
 import { motion } from "framer-motion";
 import FooterAll from "../../components/Footer";
@@ -25,19 +25,21 @@ const Profile = () => {
       exit={{ opacity: 0 }}
       transition={{ duration: 1 }}
     >
-      <HeaderAll />
-      <DivCentral>
-        <ProfileSideBar
-          score="10000"
-          image="https://ict-imgs.vgcloud.vn/2020/09/01/19/huong-dan-tao-facebook-avatar.jpg"
-          name="Lorem Ipsum"
-          email="lorem@ipsum.com"
-          phone="555555"
-          interests="Anything"
-          location="AnyPlace"
-        />
-        <ProfileTabs />
-      </DivCentral>
+      <Container>
+        <HeaderAll />
+        <DivCentral>
+          <ProfileSideBar
+            score="10000"
+            image="https://ict-imgs.vgcloud.vn/2020/09/01/19/huong-dan-tao-facebook-avatar.jpg"
+            name="Lorem Ipsum"
+            email="lorem@ipsum.com"
+            phone="555555"
+            interests="Anything"
+            location="AnyPlace"
+          />
+          <ProfileTabs />
+        </DivCentral>
+      </Container>
       <FooterAll />
     </motion.div>
   );
--- src/pages/Profile/style.js
+++ src/pages/Profile/style.js
@@ -8,7 +8,12 @@ export const DivCentral = styled.div`
   padding: 20px;
   width: 979px;
   overflow-x: hidden;
-
   background: #ffffff;
   box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  height: 95vh;
+  background-image: linear-gradient(#ffc15e, #f5ff90, #d6ffb7);
 `;
